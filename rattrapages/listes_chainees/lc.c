45b53410eabc58797a467eca43a8b4c2a8924962
YacineCC <Haouas.Yacine@hotmail.com>
2023-10-02 23:36:19 +0200

    rattrapages liste chainnees

--- rattrapages/listes_chainees/lc.c
+++ rattrapages/listes_chainees/lc.c
@@ -1,23 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
+typedef struct element element;
 
-
-typedef struct element
+struct element
 {
 	int val; // valeur entière de l'élement de la liste chaînée
-	struct element* svt;// pointeur sur le prochain élement de la liste chaînée
-}element;// élement d'une liste chaînée, pointeur sur un élement et début dela liste chaînée en court "lc"
+	element* svt;// pointeur sur le prochain élement de la liste chaînée
+};// élement d'une liste chaînée, pointeur sur un élement et début dela liste chaînée en court "lc"
+typedef element* liste;
 
-	
+void affiche(liste L)
+{
+	printf("[");
+	if(L != NULL)
+	{
+		liste l = L;
+		printf("%d", l->val);
+		l = l->svt;
+		while(l != NULL)
+		{
+			printf(", %d", l->val);
+			l = l->svt;
+		}
+		printf("]\n");
+	}
+}
+liste create(int val)
+{
+	liste l = (liste) malloc(sizeof(element));
+	l->val = val;
+	l->svt = NULL;
+	return l;
+}
+
+void add_val(liste L, int val)
+{
+	if(L == NULL)
+		return;
+	liste l = L;
+	while(l->svt != NULL)
+	{
+		l = l->svt;
+	}
+	element* tmp = (element*) malloc(sizeof(element));
+	tmp->val = val;
+	tmp->svt = NULL;
+	l->svt = tmp;
+
+
+}
 
 //void add(lc)
 
 
+
+
 int main()
 {
-	element test;
-	test.val = 1;
-	printf("%d\n",test.val);
+	liste L = create(0);
+
+	for(int i = 1; i < 30; i++)
+	{
+		add_val(L,i);
+		affiche(L);
+	}
 	return 0;
 }
